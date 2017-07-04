e36b856cb2516551925a93d479f7869f75efd1f3
ahmedmar3y2017 <ahmedmar3y108@gmail.com>
2017-07-04 15:45:36 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,36 @@
 import { Component } from '@angular/core';
+import {status} from './shared/shared'
+
 
 @Component({
+  moduleId :module.id,
   selector: 'my-app',
-  template: `<h1><app-product></app-product> Hello man {{name}}</h1>`,
+ templateUrl :"app.component.html"
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+ 
+   name :string = 'ahmed'; 
+address : string ="tanta";
+flag : boolean =true;
+arr : string[] =["ahmed" , "mohamed" , "eslam"];
+
+users: any[] =[
+{id : 1 , name : 'ahmed' , salary : 2000},
+{id : 2 , name : 'mohamed' , salary : 2000},
+{id : 3 , name : 'eslam' , salary : 2000},
+{id : 4 , name : 'ali' , salary : 2000}
+
+];
+status :status=status.available;
+
+
+
+getTitle():string{
+
+return "Done ya man : ";
+
+}
+
+
+
+}
---
+++ src/app/shared/shared.ts
@@ -0,0 +1,6 @@
+export enum status{
+
+available,
+unavailable
+
+};
